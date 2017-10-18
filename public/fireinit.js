7b5b82bb323ce54cdf4cea5531e314a7dc2c41f2
ITKOO <j4550118@gmail.com>
2017-10-18 13:36:52 +0900

    빵클락 소개 및 관리자 웹사이트

---
+++ public/fireinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDP3CPGAdLP7vp4q2JTeVtu67DI5Dzg9dE",
+  authDomain: "stac-5db0e.firebaseapp.com",
+  databaseURL: "https://stac-5db0e.firebaseio.com",
+  projectId: "stac-5db0e",
+  storageBucket: "stac-5db0e.appspot.com",
+  messagingSenderId: "937083530677"
+};
+firebase.initializeApp(config);
